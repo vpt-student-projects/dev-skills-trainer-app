bb8093d9f7e9301480214961e9b0aaee4c90a944
unmoral <maksn19092006@gmail.com>
2025-12-14 21:23:35 +0300

    the final version of the UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
 import 'users screen/auth_screen.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await dotenv.load(fileName: ".env");
+  //await dotenv.load(fileName: ".env");
 
-  await Supabase.initialize(
-    url: dotenv.env['SUPABASE_URL']!,
-    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
-  );
+  //await Supabase.initialize(
+  //  url: dotenv.env['SUPABASE_URL']!,
+  //  anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  //);
 
   runApp(const MyApp());
 }
